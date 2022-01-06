da95018a2526bb6f70d25b2a888b6d43679863f3
GhostFairy <ghhjkk@gmail.com>
2022-01-06 16:50:24 +0900

    SimpleDateFormat 수정

--- JAVA/practice/etc/Homework1_9.java
+++ JAVA/practice/etc/Homework1_9.java
@@ -7,7 +7,7 @@ import java.text.SimpleDateFormat;
 public class Homework1_9 {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println(sdf.format(new Date()));
 	}
 
