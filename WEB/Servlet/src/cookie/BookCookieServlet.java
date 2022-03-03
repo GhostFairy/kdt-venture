10a4cf1eed2e12ceedda36ad3e31c899bf2745ea
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-03-03 10:09:01 +0900

    하나의 쿠키를 수정해가며 사용하도록 변경

--- WEB/Servlet/src/cookie/BookCookieServlet.java
+++ WEB/Servlet/src/cookie/BookCookieServlet.java
@@ -14,23 +14,37 @@ import javax.servlet.http.HttpServletResponse;
 public class BookCookieServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	int bookCookieCount;
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String bookTitle = request.getParameter("title");
-		Cookie bookCookie = new Cookie("c_bookTitle" + bookCookieCount++, bookTitle);
-		response.addCookie(bookCookie);
-
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 
+		boolean isBookTitleCookieExists = false;
 		out.println("<h1>지금까지 선택하신 도서는 ");
-		for (Cookie c : request.getCookies()) {
-			if (c.getName().contains("c_bookTitle") && c.getValue() != null) {
-				out.println(c.getValue() + ", ");	
+		for (Cookie cookie : request.getCookies()) {
+			if (cookie.getName().equals("bookTitle")) {
+				isBookTitleCookieExists = true;
+				if (bookTitle != null) {
+					// 쿠키가 있음 + 입력값이 있음: 기존 쿠키에 입력값을 더해 출력
+					cookie.setValue(cookie.getValue() + "-" + bookTitle);
+					response.addCookie(cookie);
+				}
+				// 쿠키가 있음 + 입력값이 없음: 기존 쿠키 내용을 출력
+				out.println(cookie.getValue() + "입니다.</h1>");
+				break;
 			}
 		}
-		out.println(bookTitle + "입니다.</h1>");
+		if (!isBookTitleCookieExists) {
+			if (bookTitle != null) {
+				// 쿠키가 없음 + 입력값이 있음: 새 쿠키에 입력값을 넣어 출력
+				Cookie bookTitleCookie = new Cookie("bookTitle", bookTitle);
+				response.addCookie(bookTitleCookie);
+				out.println(bookTitleCookie.getValue() + "입니다.</h1>");
+			} else {
+				// 쿠키가 없음 + 입력값이 없음: 없습니다 출력
+				out.println("없습니다.</h1>");
+			}
+		}
 	}
 
 }
